3536b43e33ed18b4dbac2b66c3fb63d4cad16cf4
Rúben Carneiro <rubencarneiro01@gmail.com>
2020-05-12 22:13:29 +0100

    change discription

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -22,8 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := full_hammerhead
 PRODUCT_DEVICE := hammerhead
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on HammerHead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5 ubuntu Edition
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
