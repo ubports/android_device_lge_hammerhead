0d32d20bdfd11ff7f48b44e830458777c5ff2844
rubencarneiro <rubencarneiro01@gmail.com>
2020-07-05 13:22:40 +0100

    Change PRIVATE_BUILD_DESC

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common LineageOS stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -19,4 +19,4 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
+    PRIVATE_BUILD_DESC="hammerhead on Halium 7.1"
